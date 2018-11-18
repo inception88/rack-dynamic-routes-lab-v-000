23657b6d08aac61cdce9b2b62dd05183e4616045
Benjamin Jones <bljones36@gmail.com>
2018-11-18 21:02:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    @@item = []
+    @@items = []
 
     if req.path.match(/items/)
       item = req.path.split("/items/").last
