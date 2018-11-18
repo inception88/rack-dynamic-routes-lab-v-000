cfe62141745f4a6ea498ad807f6a62a29728f91b
Benjamin Jones <bljones36@gmail.com>
2018-11-18 21:33:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,10 @@
 class Application
 
+  @@items = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    @@items = []
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
