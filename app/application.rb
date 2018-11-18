307e2f2df9dbd3c46ae03ff929185abd8f080900
Benjamin Jones <bljones36@gmail.com>
2018-11-18 20:52:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,10 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
+        if !item.included?
+          resp.status = 400
+          resp.write "Error"
+        else
         return item.price
     else
       resp.status = 404
