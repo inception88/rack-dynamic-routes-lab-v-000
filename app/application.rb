b0516e963723a46a026f3180752303f29127aea1
Benjamin Jones <bljones36@gmail.com>
2018-11-18 21:13:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = search_items(item_name)
-      if !@@items.include?(item)
+      if item.empty?
           resp.status = 400
           resp.write "Item not found"
         else
